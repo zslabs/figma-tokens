5db175f264992aeffbbda8797c8092c6618ee64a
MightyDevKnight <takao.kamio@brandcode.dev>
2022-05-11 13:43:49 +0900

    add styles to story

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,6 +10,20 @@ module.exports = {
   webpackFinal: async (config, { configType }) => {
     config.mode = 'none';
     config.resolve.alias['@'] = path.resolve(__dirname, '../src/');
+    config.module.rules.push(
+      { test: /\.css$/, loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }]},
+      {
+        test: /\.svg$/,
+        use: [{
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [{ removeViewBox: false }]
+            }
+          }
+        }],
+      },
+    );
     return config;
   },
 }
