0be506d49da7ccdc42921b25c4fedfbde2b96120
MightyDevKnight <takao.kamio@brandcode.dev>
2022-05-13 11:54:42 +0900

    fix bug with tabbutton

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-  stories: [ "../src/**/*.stories.mdx", "../src/stories/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.stories.mdx", "../src/stories/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -8,12 +8,9 @@ module.exports = {
   ],
   framework: "@storybook/react",
   webpackFinal: async (config, { configType }) => {
-    console.log("before", config.module.rules);
     config.resolve.alias['@'] = path.resolve(__dirname, '../src/');
-    // const fileLoaderRule = config.module.rules.find((rule) => rule.test && rule.test.test( "/\.(svg|ico|jpg|jpeg|png|apng|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/"));
-    // fileLoaderRule.exclude = /\.svg$/
     config.module.rules.push(
-      { test: /\.css$/, loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }]},
+      { test: /\.css$/, loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
     );
     console.log("after", config.module.rules);
     return config;
