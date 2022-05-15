dfe90e3c0c2346d266747dd470b7a89fd40de5a6
Jan Six <six7@github.com>
2022-05-15 12:16:50 +0200

    update import

--- use-figma-tokens/index.ts
+++ use-figma-tokens/index.ts
@@ -1,7 +1,7 @@
 const {
-  getTokensStoredOnDocument, getUsedTokenSetsOnDocument, mergeTokens, getResolvedTokens,
+  getTokensStoredOnDocument, getUsedTokenSetsOnDocument, mergeTokens, getResolvedTokens, getTokensOnNode, getTokensByKey,
 } = require('./dist/useFigmaTokens');
 
 module.exports = {
-  getTokensStoredOnDocument, getUsedTokenSetsOnDocument, mergeTokens, getResolvedTokens,
+  getTokensStoredOnDocument, getUsedTokenSetsOnDocument, mergeTokens, getResolvedTokens, getTokensOnNode, getTokensByKey,
 };
